39535e617a12e8f701b7fecf2558563ebe312828
AnoMorCH <anomorch@gmail.com>
2022-01-10 08:35:25 +0300

    Little adjustments

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -139,26 +139,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+AWS_ACCESS_KEY_ID = 'AKIAT3JE54ZDVPHSEYTD'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'anomorch-poll-project-bucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_REGION_NAME = 'eu-central-1'
 
-MEDIA_URL = '/images/'
-
+AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'poll/static')
+    os.path.join(BASE_DIR, 'poll/static'),
 ]
 
-MEDIA_ROOT = os.path.join(STATICFILES_DIRS[0], 'poll/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-AWS_ACCESS_KEY_ID = 'AKIAT3JE54ZDXSHR5CGW'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'anomorch-poll-project'
-
-AWS_S3_REGION_NAME = 'us-west-2'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
